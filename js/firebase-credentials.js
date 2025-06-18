1b8f13843715624cbc8641bc0423c492dc94b870
Maxine <maxine.audin@gmail.com>
2025-06-18 09:44:09 +0200

    remove firebase-credentials

--- js/firebase-credentials.js
+++
@@ -1,5 +0,0 @@
-// set parameters to log data to a keen.io project.
-var firebase_config = {
-	apiKey: "AIzaSyBwDr8n-RNCbBOk1lKIxw7AFgslXGcnQzM",
-	databaseURL: "https://marineexpe.firebaseio.com/"
-}
