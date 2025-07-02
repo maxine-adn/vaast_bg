7a4f879efcac6496a4be2bc2a41eff5909b7c75d
Maxine <maxine.audin@gmail.com>
2025-07-02 11:27:40 +0200

    changed iat to vaast in div id

--- js/vaast-plugins/jspsych-vaast-fixation.js
+++ js/vaast-plugins/jspsych-vaast-fixation.js
@@ -67,7 +67,7 @@ var jsPsychVaastFixation = (function(jspsych) {
       let html_str = "";
     
       html_str += "<div style='position: absolute; right: 0; top: 0; width: 100%; height: 100%; background: url(" + trial.background_images[trial.position] + ") center no-repeat; z-index: -1; background-color: #000000'></div>";
-      html_str += "<div style='height: 100vh; display: flex; justify-content: center; align-items: center; z-index:1; color: #ffffff; font-size: " + trial.font_size + "px' id='jspsych-iat-stim'>" + trial.fixation + "</div>";
+      html_str += "<div style='height: 100vh; display: flex; justify-content: center; align-items: center; z-index:1; color: #ffffff; font-size: " + trial.font_size + "px' id='jspsych-vaast-stim'>" + trial.fixation + "</div>";
       
       display_element.innerHTML = html_str;
       
